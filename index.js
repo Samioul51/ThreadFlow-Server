abd873fc02f7c1f37d243bd3d6e0bcd2585090e0
Samioul51 <akmsamioulislam51@gmail.com>
2025-12-09 01:39:50 +0600

    API for single product created

--- index.js
+++ index.js
@@ -46,6 +46,26 @@ async function run() {
       
     });
 
+    // Single Product
+
+    app.get("/products/:id",async(req,res)=>{
+      try{
+        const id=req.params.id;
+        const item=await products.findOne({_id:new ObjectId(id)});
+
+        res.send({
+          success:true,
+          data:item
+        });
+
+      }catch(error){
+        res.status(500).send({
+          success:false,
+          message:error.message
+        })
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
