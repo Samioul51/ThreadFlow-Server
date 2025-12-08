5370fc709263471a6e05f4efd1a47e9c18b48c01
Samioul51 <akmsamioulislam51@gmail.com>
2025-12-08 19:37:37 +0600

    Connected With MongoDB

---
+++ index.js
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.user_name}:${process.env.password}@cluster0.tugpfto.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Server running...')
+})
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+})
