ea6d418b94d9bef7adda1104e859ef59b6aa7c91
Samioul51 <akmsamioulislam51@gmail.com>
2025-12-10 00:31:30 +0600

    Stripe API added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
+import Stripe from 'stripe';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const stripe = new Stripe(process.env.stripe_secret_key);
 
 app.use(cors());
 app.use(express.json());
@@ -37,40 +39,62 @@ async function run() {
           success: true,
           data: list
         });
-      } catch(error){
+      } catch (error) {
         res.status(500).send({
-          success:false,
-          message:error.message
+          success: false,
+          message: error.message
         })
       }
-      
+
     });
 
     // Single Product
 
-    app.get("/products/:id",async(req,res)=>{
-      try{
-        const id=req.params.id;
-        const item=await products.findOne({_id:new ObjectId(id)});
+    app.get("/products/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const item = await products.findOne({ _id: new ObjectId(id) });
 
         res.send({
-          success:true,
-          data:item
+          success: true,
+          data: item
         });
 
-      }catch(error){
+      } catch (error) {
         res.status(500).send({
-          success:false,
-          message:error.message
+          success: false,
+          message: error.message
         })
       }
     });
+
+    // Stripe
+
+    app.post("/create-payment-intent", async (req, res) => {
+      try {
+        const { amount } = req.body;
+
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount * 100,
+          currency: "bdt",
+          automatic_payment_methods: { enabled: true }
+        });
+
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        });
+
+      } catch (error) {
+        res.status(500).send({ error: error.message })
+      }
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // await client.close();
   }
+
 }
 run().catch(console.dir);
 
